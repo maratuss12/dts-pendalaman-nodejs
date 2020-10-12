881f6cfffd279fcbafe3a0e8de01258f7999a1ac
maratus_sholekhah <maratuss12@gmail.com>
2020-10-13 00:12:32 +0700

    init body parser & handle form data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 const app = express()
 const __dirname =path.resolve()
@@ -13,6 +14,9 @@ app.engine('html', hbs.__express)
 //log incoming request
 app.use(morgan('combined'))
 
+//parse request body
+app.use(bodyParser.urlencoded())
+
 //serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
 
@@ -22,6 +26,17 @@ app.get('/', (req, res, next)=>{
 
 app.get('/product', (req, res, next)=>{
     res.render('product')
+})
+
+//handle form GET method
+app.get('/add-product', (req, res, next)=>{
+    res.send(req.query)
+})
+
+//handle form POST method
+app.post('/add-product', (req, res, next)=>{
+    console.log('Request body', req.body)
+    res.send(req.body)
 })
 
 app.use((err, req, res, next)=>{
