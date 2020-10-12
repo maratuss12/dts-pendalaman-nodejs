75aabf72821fba5d40bd9d0742fb2d9fbe8edde3
maratus_sholekhah <maratuss12@gmail.com>
2020-10-12 22:50:28 +0700

    init layout & serve static files

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
+import morgan from 'morgan'
 
 const app = express()
 const __dirname =path.resolve()
 
 app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
-app.set('html', hbs.__express)
+app.engine('html', hbs.__express)
+
+//log incoming request
+app.use(morgan('combined'))
+
+//serve static file
+app.use('/assets', express.static(__dirname + '/assets'))
 
 app.get('/', (req, res, next)=>{
     res.send({success: true})
+})
+
+app.get('/product', (req, res, next)=>{
+    res.render('product')
 })
 
 app.use((err, req, res, next)=>{
